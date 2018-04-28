97f5f6fa5d01e4e6073482a75da9da7462582703
Anagix Support <support@anagix.com>
2018-04-28 20:42:48 +0900

    Used OpenRule1um process executed at FullAdder_or1u directory

--- synthesis/fa.spc
+++ synthesis/fa.spc
@@ -1,16 +1,23 @@
 *SPICE netlist created from BLIF module fa by blif2BSpice
-.include /usr/local/share/qflow/tech/osu035/osu035_stdcells.sp
+.include /usr/local/share/qflow/tech/OpenRule1um/OpenRule1um_stdcells.sp
 .subckt fa vdd gnd ck rst a b ci s co 
-XAND2X2_1 vdd gnd a ci _5_ AND2X2
-XOAI21X1_1 gnd vdd _4_ _5_ _6_ b OAI21X1
-XINVX1_1 b _7_ vdd gnd INVX1
-XOR2X2_1 _1_ ci vdd gnd a OR2X2
-XNAND2X1_1 vdd _2_ gnd a ci NAND2X1
-XNAND3X1_1 _2_ vdd gnd _7_ _1_ _3_ NAND3X1
-XAOI21X1_1 gnd vdd _3_ _6_ _0_ rst AOI21X1
-XBUFX2_1 vdd gnd gnd co BUFX2
-XBUFX2_2 vdd gnd _8_ s BUFX2
-XDFFPOSX1_1 vdd _0_ gnd _8_ ck DFFPOSX1
-XNOR2X1_1 vdd ci gnd _4_ a NOR2X1
+XINVX1_1 rst _11_ vdd gnd INVX1
+XINVX1_2 ci _12_ vdd gnd INVX1
+XINVX1_3 a _13_ vdd gnd INVX1
+XNAND2X1_1 vdd _14_ gnd _15_ _1_ NAND2X1
+XNAND2X1_2 vdd _1_ gnd ci a NAND2X1
+XNAND2X1_3 vdd _2_ gnd _3_ _2_ NAND2X1
+XNAND2X1_4 vdd _3_ gnd b _4_ NAND2X1
+XINVX1_4 b _4_ vdd gnd INVX1
+XNAND2X1_5 vdd _5_ gnd ci _1_ NAND2X1
+XNAND2X1_6 vdd _6_ gnd a _15_ NAND2X1
+XNAND2X1_7 vdd _7_ gnd _7_ _8_ NAND2X1
+XNAND2X1_8 vdd _8_ gnd _6_ _9_ NAND2X1
+XNAND2X1_9 vdd _9_ gnd _5_ _10_ NAND2X1
+XNAND2X1_10 vdd _10_ gnd _14_ _11_ NAND2X1
+XINVX1_5 _12_ _0_ vdd gnd INVX1
+XBUFX2_1 vdd gnd _15_ s BUFX2
+XBUFX2_2 vdd gnd gnd co BUFX2
+XDFFPOSX1_1 vdd _0_ gnd _15_ ck DFFPOSX1
 .ends fa
  
---
+++ synthesis/fa_tb.v
@@ -0,0 +1,46 @@
+// -----------------------------------------------------------------------------
+//  Title         : 1bit full-adder with output latch (test bench)
+//  Project       : 
+// -----------------------------------------------------------------------------
+//  File          : fa_tb.v
+//  Author        : 
+//  Created       : 2018/ 3/xx
+//  Last modified : 
+// -----------------------------------------------------------------------------
+//  Description   : 1bit full-adder with output latch
+// -----------------------------------------------------------------------------
+//  Copyright (C) 2018 ??? All Rights Reserved.
+// -----------------------------------------------------------------------------
+
+`timescale  1ns/1ps
+
+module fa_tb();
+    reg             ck, rst;
+    reg     [2:0]   i;
+    wire            s, co, a, b, ci;
+
+    assign {a, b, ci} = i;
+   
+    always begin
+        #50; ck = ~ck;
+    end
+
+    initial begin
+        $dumpfile("fa_tb.vcd");
+        $dumpvars(0,fa_tb);
+
+        ck <= 0; rst <= 1; i <= 3'b000;
+        #100; rst <= 0;
+        #100; i <= 3'b001;
+        #100; i <= 3'b010;
+        #100; i <= 3'b011;
+        #100; i <= 3'b100;
+        #100; i <= 3'b101;
+        #100; i <= 3'b110;
+        #100; i <= 3'b111;
+        #500 $finish;
+    end
+
+    fa fa(.ck(ck), .rst(rst), .a(a), .b(b), .ci(ci), .s(s), .co(co));
+
+endmodule
